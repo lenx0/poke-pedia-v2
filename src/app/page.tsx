34ce4c392de0a636f952fa913a53a44bd114fe81
Thiago Beraldo <thiagofreitas1106@gmail.com>
2024-11-24 17:56:01 -0300

    home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,202 @@
+"use client";
+
+import { Box, Button, Typography } from "@mui/material";
+import { motion } from "framer-motion";
+import '@fontsource/russo-one';
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Box
+      sx={{
+        width: "100vw",
+        minHeight: "100vh",
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        overflow: "hidden",
+      }}
+    >
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      {/* left-decoration */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 4 }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "-50px",
+            left: "-50px",
+            width: "200px",
+            height: "200px",
+            borderRadius: "50%",
+            background: "rgba(255, 0, 0, 0.527)",
+            zIndex: 0,
+          }}
+        />
+      </motion.div>
+
+      {/* right-decoration */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 4 }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "-50px",
+            right: "-50px",
+            width: "200px",
+            height: "200px",
+            borderRadius: "50%",
+            background: "rgba(183, 219, 23, 0.623)",
+            zIndex: 0,
+          }}
+        />
+      </motion.div>
+
+      {/* title */}
+      <Box sx={{ paddingTop: "50px", zIndex: 1 }}>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 2 }}
+        >
+          <Typography
+            variant="h1"
+            sx={{
+              textAlign: "center",
+              marginTop: "20px",
+              fontSize: { lg: "7.5rem", xs: "2.5rem", sm: "3.5rem" },
+            }}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
+            Pokémon Pédia
+          </Typography>
+        </motion.div>
+
+        {/* subtitle-1 */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2, delay: 2 }}
+        >
+          <Typography
+            variant="h6"
+            sx={{
+              textAlign: "center",
+              marginTop: "10px",
+              fontSize: { xs: "1rem", sm: "1.25rem" },
+
+            }}
           >
-            Read our docs
-          </a>
+            O guia definitivo para treinadores e entusiastas Pokémon!
+          </Typography>
+        </motion.div>
+
+        {/* subtitle-2 */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2, delay: 4 }}
+        >
+          <Typography
+            variant="h5"
+            sx={{
+              textAlign: "center",
+              marginTop: "10px",
+            }}
+          >
+            Explore o universo Pokémon, conheça as criaturas e suas habilidades!
+          </Typography>
+        </motion.div>
+
+        <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 10 }}
+        >
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <Button variant="contained">Explorar</Button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        </motion.div>
+        
+      </Box>
+
+      {/* float-button */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 8 }}
+      >
+        <Button
+          href="/explore"
+          sx={{
+            position: "absolute",
+            bottom: "20px",
+            right: "20px",
+            padding: 0,
+            minWidth: "unset",
+            backgroundColor: "transparent",
+            border: "none",
+            animation: "bounce 1.5s infinite",
+            "&:hover": {
+              transform: "scale(1.1)",
+            },
+          }}
         >
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/images/pokeball.png"
+            alt="Entrar"
+            width={80}
+            height={80}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        </Button>
+      </motion.div>
+
+      {/* Footer */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 6 }}
+      >
+        <Box
+          component="footer"
+          sx={{
+            width: "100%",
+            textAlign: "center",
+            backgroundColor: "#3a3a3fe6",
+            padding: "10px 20px",
+            color: "white",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Typography variant="h6">
+            Este site utiliza imagens e informações oficiais de propriedade da{" "}
+            <strong>The Pokémon Company</strong>. Todos os direitos são reservados aos seus respectivos proprietários.
+          </Typography>
+        </Box>
+      </motion.div>
+
+      <style jsx global>{`
+          @keyframes bounce {
+            0%,
+            20%,
+            50%,
+            80%,
+            100% {
+              transform: translateY(0);
+            }
+            40% {
+              transform: translateY(-20px);
+            }
+            60% {
+              transform: translateY(-10px);
+            }
+          }
+        `}</style>
+    </Box>
   );
 }
---
+++ src/theme/index.js
@@ -0,0 +1,92 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFCB05",
+      contrastText: "#2A75BB",
+    },
+    secondary: {
+      main: "#2A75BB",
+      contrastText: "#FFCB05",
+    },
+    background: {
+      default: "#ffffff",
+      paper: "#ed6b2f", // definir cor
+    },
+    text: {
+      primary: "#333333",
+      secondary: "#656565",
+    },
+  },
+  typography: {
+    // fontFamily: "'Press Start 2P', 'Roboto', 'Arial', sans-serif", - font estilo gameboy
+    fontFamily: "Russo One, Arial, sans-serif",
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 500,
+      color: "#333333"
+    },
+    h2: {
+      fontSize: "2.5rem",
+      fontWeight: 700,
+      color: "#FFCB05"
+    },
+    h5: {
+      color: "#656565",
+      fontSize: "1rem"
+    },
+    h6: {
+      letterSpacing: 2,
+      lineHeight: 2,
+      fontSize: "0.8rem"
+    },
+    body1: {
+      fontSize: "1.2rem",
+      color: "#ffffff",
+      lineHeight: 1.6
+    },
+    button: {
+      textTransform: "uppercase",
+      width: "100px",
+      height: "100px"
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "100%",
+          padding: "12px 24px",
+          fontWeight: "bold"
+        },
+        containedPrimary: {
+          backgroundColor: "#ffcd05ff",
+          color: "#000000",
+          "&:hover": {
+            backgroundColor: "#f20505",
+            color: "#ffffff",
+          },
+        },
+        containedSecondary: {
+          backgroundColor: "#2A75BB",
+          color: "#FFCB05",
+          "&:hover": {
+            backgroundColor: "#1E5FA0",
+          },
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          borderRadius: "16px",
+          padding: "16px",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
